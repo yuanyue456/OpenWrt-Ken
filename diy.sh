937fa42119efee3e35ece1d23d6527804f18b6f5
yuanyue456 <doinmyself@hotmail.com>
2020-04-16 13:24:08 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,6 +7,7 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/#src-git/src-git/g' feeds.conf.default
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
 ./scripts/feeds update -a
 ./scripts/feeds install -a
